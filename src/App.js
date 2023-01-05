e315c2523e094618e22239c59cab0773e0d0b3b6
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2023-01-05 21:38:44 +0100

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import { Game } from './components/Game';
 import { Home } from "./components/Home"
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route index element={<Home />}></Route>
           <Route path="/game" element={<Game />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
