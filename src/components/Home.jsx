64e443e5c35274ab7e8b25c87c714cf9e8912230
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2023-01-06 12:44:19 +0100

    css och fontfix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -287,16 +287,12 @@ export function Home() {
             </div>
         </div>
 
-        
-
-        <span className="information animate__animated animate__fadeInUp">
-            This mobile game was made for my thesis assignment at Medieinstitutet. Its a game where you guess the results of all the fixtures in the upcoming round of the Premier League.
-
-            You swipe the card to the side you think will win the fixture, and swipe up if you think it will be a draw.
-
-            You have 20 seconds to guess all the fixtures, hurry up and try to guess them all right and chase a highscore to brag about to your friends!
-            <a href="https://github.com/GamerShabandari" target="_blank" rel="noreferrer">Gamer S<FaGithubSquare className="github"></FaGithubSquare></a>
-
-        </span>
+        <footer>
+            <a href="https://github.com/GamerShabandari" target="_blank" rel="noreferrer" className="animate__animated animate__heartBeat animate__infinite	infinite">
+                <LinearGradient gradient={['to left', '#17acff ,#ff68f0']}>
+                    Gamer Shabandari ©
+                </LinearGradient>
+            </a>
+        </footer>
     </>)
 }
