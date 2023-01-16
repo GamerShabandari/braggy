d693e4433e922631bd56b62d6346fa43af6fe537
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2023-01-16 09:07:17 +0100

    favicon and new stagger bounce animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -520,7 +520,14 @@ export function Home() {
             <motion.div className="resultListFixture" key={i}
                 initial={{ opacity: 0, translateY: -20 }}
                 animate={{ opacity: 1, translateY: 0 }}
-                transition={{ ease: "easeInOut", duration: 0.2, delay: i * 0.2 }}
+                transition={{ 
+                    type: "spring",
+                    stiffness: 217,
+                    mass: 1,
+                    damping: 5,
+                    duration: 0.3, 
+                    delay: i * 0.3 
+                }}
             >
                 <div className="hometeamContainer">
                     <img src={"./img/" + fixt.homeTeam + ".png"} draggable={false} alt="hometeam logo" />
